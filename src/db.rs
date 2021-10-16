635d4bfefc179160a76191091c2af40595c0ef69
Ryuichi-g <hsatlefp@gmail.com>
2021-10-17 06:02:29 +0900

    エラー処理リファクタリング

--- src/db.rs
+++ src/db.rs
@@ -12,7 +12,7 @@ pub async fn get_todos(client: &Client) -> Result<Vec<TodoList>, AppError> {
 
     let todos = client.query(&statement, &[])
         .await
-        .expect("Error getting todo lists")
+        .map_err(AppError::db_error)?
         .iter()
         .map(|row| TodoList::from_row_ref(row).unwrap())
         .collect::<Vec<TodoList>>();
@@ -29,7 +29,7 @@ pub async fn get_items(client: &Client, list_id: i32) -> Result<Vec<TodoItem>, A
 
     let items = client.query(&statement, &[&list_id])
         .await
-        .expect("Error getting todo lists")
+        .map_err(AppError::db_error)?
         .iter()
         .map(|row| TodoItem::from_row_ref(row).unwrap())
         .collect::<Vec<TodoItem>>();
@@ -45,7 +45,7 @@ pub async fn create_todo(client: &Client, title: String) -> Result<TodoList, App
 
     client.query(&statement, &[&title])
         .await
-        .expect("Error creating todo list")
+        .map_err(AppError::db_error)?
         .iter()
         .map(|row| TodoList::from_row_ref(row).unwrap())
         .collect::<Vec<TodoList>>()
@@ -65,7 +65,7 @@ pub async fn check_item(client: &Client, list_id: i32, item_id: i32) -> Result<b
 
     let result = client.execute(&statement, &[&list_id, &item_id])
         .await
-        .expect("Error checking todo item");
+        .map_err(AppError::db_error)?;
 
     match result {
         ref updated if *updated == 1 => Ok(true),
--- src/handlers.rs
+++ src/handlers.rs
@@ -3,8 +3,7 @@ use crate::db;
 use crate::errors::AppError;
 use deadpool_postgres::{Pool, Client};
 use actix_web::{web, Responder, HttpResponse};
-use slog::{Logger, o, crit};
-
+use slog::{Logger, o, crit, error};
 
 pub async fn get_client(pool: Pool, log: Logger) -> Result<Client, AppError> {
     pool.get().await
@@ -14,6 +13,14 @@ pub async fn get_client(pool: Pool, log: Logger) -> Result<Client, AppError> {
             
             AppError::db_error(err)
         })
+}
+
+pub fn log_error(log: Logger) -> Box<dyn Fn(AppError) -> AppError> {
+    Box::new( move |err| {
+        let sublog = log.new(o!("cause" => err.cause.clone()));
+        error!(sublog, "{}", err.message()); 
+        err
+    })
 }
 
 pub async fn status() -> impl Responder { 
@@ -29,13 +36,14 @@ pub async fn get_todos(state: web::Data<AppState>) -> Result<impl Responder, App
     let result = db::get_todos(&client).await;
 
     result.map(|todos| HttpResponse::Ok().json(todos))
+        .map_err(log_error(log))
 }
 
 pub async fn get_items(state: web::Data<AppState>, path: web::Path<(i32, )>) -> Result<impl Responder, AppError> {
 
-    let client: Client = 
-        state.pool.get().await
-        .map_err(AppError::db_error)?;
+    let log = state.log.new(o!("handlers" => "get_items"));
+    
+    let client: Client = get_client(state.pool.clone(), log.clone()).await?;
 
 
     let result = db::get_items(&client, path.0).await;
@@ -45,24 +53,25 @@ pub async fn get_items(state: web::Data<AppState>, path: web::Path<(i32, )>) -> 
 
 pub async fn create_todo(state: web::Data<AppState>,  json: web::Json<CreateTodoList>) -> Result<impl Responder, AppError> {
 
+    let log = state.log.new(o!("handlers" => "create_todo"));
     
-    let client: Client = 
-        state.pool.get().await
-        .map_err(AppError::db_error)?;
-
+    let client: Client = get_client(state.pool.clone(), log.clone()).await?;
 
     let result = db::create_todo(&client, json.title.clone()).await;
 
     result.map(|todo| HttpResponse::Ok().json(todo))
+        .map_err(log_error(log))
+
 }
 
 pub async fn check_item(state: web::Data<AppState>,  path: web::Path<(i32, i32)>) -> Result<impl Responder, AppError> {
 
-    let client: Client = 
-        state.pool.get().await
-        .map_err(AppError::db_error)?;
+    let log = state.log.new(o!("handlers" => "check_item"));
+
+    let client: Client = get_client(state.pool.clone(), log.clone()).await?;
 
     let result = db::check_item(&client, path.0, path.1).await;
 
     result.map(|updated| HttpResponse::Ok().json(ResultResponse{success: updated}))
+        .map_err(log_error(log))
 }
