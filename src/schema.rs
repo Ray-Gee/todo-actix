eab5372ffe9fd42cc1029fafe25381a627eb7322
Ryuichi-g <hsatlefp@gmail.com>
2021-10-20 08:57:05 +0900

    usersテーブル作成をdieselで

--- src/schema.rs
+++ src/schema.rs
@@ -14,9 +14,24 @@ table! {
     }
 }
 
+table! {
+    users (id) {
+        id -> Uuid,
+        username -> Varchar,
+        email -> Varchar,
+        password_hash -> Varchar,
+        full_name -> Nullable<Varchar>,
+        bio -> Nullable<Varchar>,
+        image -> Nullable<Varchar>,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
+
 joinable!(todo_item -> todo_list (list_id));
 
 allow_tables_to_appear_in_same_query!(
     todo_item,
     todo_list,
+    users,
 );
